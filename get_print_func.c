2d418ddda28b86e29d3ca80d0383e789cb5d1fd0
Sammyregt <adeoyesamuel100@gmail.com>
2023-01-23 08:53:20 +0000

    update file

--- get_print_func.c
+++ get_print_func.c
@@ -9,7 +9,7 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
 {
 	print_t pr[] = {
 		{"c", print_chr}, {"s", print_str},
-		{"%", print_prg},
+		{"%", print_prg}, {" %", print_prg},
 	};
 	int i = 0, j = 0, first_index;
 
