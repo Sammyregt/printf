f938cd1964db7e23cc40aca30b3c4620a58b027d
Sammyregt <adeoyesamuel100@gmail.com>
2023-01-23 16:24:16 +0000

    update: observe betty rules

--- get_print_func.c
+++ get_print_func.c
@@ -34,7 +34,7 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
 		{"l", print_prg}, {"h", print_prg},
 		{" +i", prinpint}, {" +d", prinpint},
 		{"+ i", prinpint}, {"+ d", prinpint},
-		{" %", print_prg}, {NULL, NULL}, 
+		{" %", print_prg}, {NULL, NULL},
 	};
 	int i = 0, j = 0, first_index;
 
